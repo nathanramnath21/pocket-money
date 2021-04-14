abcb7d6dda9026e1af470d43b5b9a39b5680caeb
nathanramnath21 <71851194+nathanramnath21@users.noreply.github.com>
2021-04-14 14:35:06 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,7 @@
+pocketmoney=int(input("how much pocketmoney do I have"))
+if (pocketmoney>500):
+    print("pocket is full")
+elif (pocketmoney>100):
+    print("pocket is half full")
+else: 
+    print("pocket is empty")
